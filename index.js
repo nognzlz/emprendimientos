55ee01eb1530fda1499868011af4ff3bd1e7d486
Nicolas Gonzalez Parra <v-ngonzalesparra@expedia.com>
2018-04-24 00:39:53 -0300

    fix route

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static('public'))
-  .get('/', (req, res) => res.render('index.html'))
-.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const app = express();
+
+app.use(express.static('public'));
+//app.get('/', (req, res) => res.render('index.html'));
+
+app.use(function(req, res) {
+  // Use res.sendfile, as it streams instead of reading the file into memory.
+  res.sendfile(__dirname + '/public/index.html');
+});
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
