1d996307a2be55634ba88ffb5016fe77cbaf249e
Nicolas Gonzalez Parra <v-ngonzalesparra@expedia.com>
2018-04-24 02:05:55 -0300

    un poquito de funcionalidad

--- public/sections/seleccion/colaboradores-controller.js
+++ public/sections/seleccion/colaboradores-controller.js
@@ -4,6 +4,26 @@
 
     function colaboradoresController (){
         var vm = this;
+
+        //valores
+        vm.cargado = false;
+
+        //functiones
+        vm.cargarColaborador = cargarColaborador;
+        vm.volver = volver;
+
+        function initialize() {
+            vm.cargado = false;
+        }
+
+        function volver() {
+            vm.cargado = false;
+        }
+
+        function cargarColaborador() {
+            vm.cargado = true;
+        }
+
         return vm;
     }
 })();
